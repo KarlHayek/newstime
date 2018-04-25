e15c99e9b23ab608122d298bd5d97fa9c9fecc52
Tarek <mbpr@TPT.local>
2018-04-25 21:22:19 +0300

    added json printing capability

--- fetcher_labeler_classifier/labeler.py
+++ fetcher_labeler_classifier/labeler.py
@@ -4,7 +4,7 @@
 # think about labeling multiple urls concurrently
 # output labels directly to the classifier component without the need for a file
 
-import textrazor
+import textrazor, json
 
 textrazor.api_key = "5634740d9e8d89a14374edaa305c207bd6eda5918bbe233634beb092"
 
@@ -12,22 +12,44 @@ class Labeler:
 
     howManyTopics = 15
     howManyEntities = 15
-
+    dict = {'TOPICS':[],
+            'ENTITIES':[],
+            'URL':[]}
 
     def __init__(self):
         self.client = textrazor.TextRazor(extractors=["topics", "entities"])  #instance of TextRazor class
         self.list = []
 
-    def extractAndPrint(self, url):
+
+    def extractIntoList(self, url):
         self.response = self.client.analyze_url(url)
 
         self.list.append("#TOPICS")
         for topic in self.response.topics()[1:self.howManyTopics]:
             self.list.append(topic.label)
+            self.dict['TOPICS'].append(topic.label)
         self.list.append("#ENTITIES")
 
         for entity in self.response.entities()[1:self.howManyEntities]:
             self.list.append(entity.id)
+            self.dict['ENTITIES'].append(entity.id)
         self.list.append("#END")
+        self.dict['URL'].append(url)
 
         return self.list
+
+
+    def printJSON(self):
+
+        with open('labels.json', 'w') as outfile:
+            json.dump(self.dict, outfile)
+        print (json.dumps(self.dict, indent=4, sort_keys=True))
+
+
+# pair enumerator component, enumerates pairs or urls from a list, do function that could be "do", and a function that only enumerates
+# this can be used also to compare keywords: separation of concerns. rewriting the thing is refactoring
+
+# work on sorting by time (temporal entity extraction, normalization, ordering). use article writing time as reference
+# tell other subteam to talk to prof
+
+# use software connectors between javascript and python. you need a system connection, or a network connection, or a file (te3tir)
--- fetcher_labeler_classifier/test.py
+++ fetcher_labeler_classifier/test.py
@@ -1,17 +1,21 @@
 import labeler
 import classifier
+import json
 
 
 l1 = labeler.Labeler()
-list1 = l1.extractAndPrint("http://money.cnn.com/2018/03/21/technology/mark-zuckerberg-cambridge-analytica-response/index.html")
+list1 = l1.extractIntoList("http://money.cnn.com/2018/03/21/technology/mark-zuckerberg-cambridge-analytica-response/index.html")
 print(list1)
 print("\n")
 
+l1.printJSON()
 
-l2 = labeler.Labeler()
-list2 = l2.extractAndPrint("https://www.nytimes.com/2018/03/27/world/europe/whistle-blower-data-mining-cambridge-analytica.html")
-print(list2)
-print("\n")
 
-c1 = classifier.Classifier()
-c1.readAndCompare(list1, list2)
+
+# l2 = labeler.Labeler()
+# list2 = l2.extractAndPrint("https://www.nytimes.com/2018/03/27/world/europe/whistle-blower-data-mining-cambridge-analytica.html")
+# print(list2)
+# print("\n")
+#
+# c1 = classifier.Classifier()
+# c1.readAndCompare(list1, list2)
